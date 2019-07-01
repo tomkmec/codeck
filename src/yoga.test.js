be5b2aecad96dc1194fc88049b6c5de4c6a83d04
Tom Kmec <tom.kmec@gmail.com>
2019-07-01 20:25:16 +0200

    Added yoga layout as dependency, with a simple test

---
+++ src/yoga.test.js
@@ -0,0 +1,34 @@
+import yoga, {Node} from 'yoga-layout-prebuilt';
+
+// https://github.com/facebook/yoga/blob/master/javascript/sources/entry-common.js
+
+describe('Page.parseLayoutDef', () => {
+	const root = Node.create();
+	root.setWidth(500);
+	root.setHeight(300);
+	root.setJustifyContent(yoga.JUSTIFY_CENTER);
+
+	const node1 = Node.create();
+	node1.setWidth(100);
+	node1.setHeight(100);
+
+	const node2 = Node.create();
+	node2.setWidth(100);
+	node2.setHeight(100);
+
+	root.insertChild(node1, 0);
+	root.insertChild(node2, 1);
+
+	root.setFlexDirection(yoga.FLEX_DIRECTION_ROW)
+	root.calculateLayout(500, 300, yoga.DIRECTION_LTR);
+	console.log(root.getComputedLayout());
+	// {left: 0, top: 0, width: 500, height: 300}
+	console.log(node1.getComputedLayout());
+	// {left: 150, top: 0, width: 100, height: 100}
+	console.log(node2.getComputedLayout());
+	// {left: 250, top: 0, width: 100, height: 100}
+
+	test('yoga', () => {
+		expect(node2.getComputedLayout().left).toBe(250)
+	})
+})
